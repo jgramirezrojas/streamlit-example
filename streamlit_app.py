d8230faadf3032977d65e7c3899838c92ac551ff
Juan Gabriel Ramirez <jgramirez@uniandes.edu.co>
2023-05-23 21:41:47 -0500

    Update streamlit_app.py
    my app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,74 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-"""
-# Welcome to Streamlit!
+# The sectors in the report
+sectors = ['Apparel, Leather & Allied Products', 'Furniture & Related Products', 'Wood Products', 
+           'Fabricated Metal Products', 'Machinery', 'Computer & Electronic Products', 'Transportation Equipment', 
+           'Plastics & Rubber Products', 'Paper Products', 'Chemical Products', 'Petroleum & Coal Products', 
+           'Primary Metals', 'Textile Mills', 'Electrical Equipment, Appliances & Components', 
+           'Food, Beverage & Tobacco Products', 'Miscellaneous Manufacturing', 'Nonmetallic Mineral Products', 
+           'Printing & Related Support Activities']
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+def process_text(text):
+    growth_sectors = text.split("are:")[1].split("The")[0].split(';')
+    contraction_sectors = text.split("contraction in")[1].split(", in the following order, are:")[1].split('.')
+    contraction_sectors = contraction_sectors[0].split(';')
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    growth_sectors = [s.strip() for s in growth_sectors]
+    contraction_sectors = [s.strip() for s in contraction_sectors]
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    # Remove 'and' from the last sector in both lists
+    if growth_sectors[-1].startswith('and '):
+        growth_sectors[-1] = growth_sectors[-1][4:]
+    if contraction_sectors[-1].startswith('and '):
+        contraction_sectors[-1] = contraction_sectors[-1][4:]
+
+    # Remove trailing period from the last sector in both lists
+    if growth_sectors[-1].endswith('.'):
+        growth_sectors[-1] = growth_sectors[-1][:-1]
+    if contraction_sectors[-1].endswith('.'):
+        contraction_sectors[-1] = contraction_sectors[-1][:-1]
+
+    ranks = {}
+    # Search the sector name in the text and assign the rank
+    for i, sector in enumerate(growth_sectors[::-1]):  # Reverse the list to give higher ranks to sectors mentioned first
+        ranks[sector] = {'Rank': i + 1, 'Status': 'Growth'}
+
+    for i, sector in enumerate(contraction_sectors[::-1]):  # Reverse the list
+        ranks[sector] = {'Rank': i + 1, 'Status': 'Contraction'}
+
+    for sector in sectors:
+        if sector not in ranks:
+            ranks[sector] = {'Rank': 0, 'Status': 'No Change'}
+
+    return ranks
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+def plot_data(df):
+    df['Color'] = df['Status'].apply(lambda x: 'green' if x == 'Growth' else ('red' if x == 'Contraction' else 'gray'))
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    plt.figure(figsize=(8, 15))
+    chart = sns.barplot(y='Sector', x='Rank', data=df, palette=df['Color'])
+    return chart
 
-    points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+    plt.figure(figsize=(15, 8))
+    chart = sns.barplot(x='Sector', y='Rank', data=df, palette=df['Color'])
+    chart.set_xticklabels(chart.get_xticklabels(), rotation=90)
+    return chart
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Streamlit interface
+st.title('ISM Manufacturing Index Dashboard')
+
+text_input = st.text_area("Enter text:", value='The five manufacturing industries that reported growth in April are: Printing & Related Support Activities; Apparel, Leather & Allied Products; Petroleum & Coal Products; Fabricated Metal Products; and Transportation Equipment. The 11 industries reporting contraction in April, in the following order, are: Furniture & Related Products; Wood Products; Nonmetallic Mineral Products; Electrical Equipment, Appliances & Components; Plastics & Rubber Products; Chemical Products; Machinery; Primary Metals; Computer & Electronic Products; Food, Beverage & Tobacco Products; and Miscellaneous Manufacturing.', height=200, max_chars=None, key=None)
+
+if st.button('Process Text'):
+    ranks = process_text(text_input)
+    df = pd.DataFrame.from_dict(ranks, orient='index')
+    df = df.reset_index().rename(columns={'index': 'Sector'})
+    chart = plot_data(df)
+    st.pyplot(chart.figure)
+    st.write("Table with ranks")
+    st.dataframe(df)
